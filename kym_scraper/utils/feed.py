0806dd74908a1fe2d830db71949f666cb078acaf
1gni5 <jules.ducange@posteo.net>
2023-11-14 10:03:09 +0100

    ♻️ Wrapped logic inside a function.

--- kym_scraper/utils/feed.py
+++ kym_scraper/utils/feed.py
@@ -1,13 +1,17 @@
 import redis
 
-NUMBER_OF_PAGES = 5
 
-# Create a redis client
-client = redis.from_url("redis://localhost:6379")
+def feed(number_of_pages, redis_url):
+    # Create a redis client
+    client = redis.from_url(redis_url)
 
-# Push the urls to the redis queue
-for page in range(1, NUMBER_OF_PAGES + 1):
-    client.lpush(
-        "kym_bootstrap_queue:start_urls",
-        f"https://knowyourmeme.com/memes/popular/page/{page}",
-    )
+    # Push the urls to the redis queue
+    for page in range(1, number_of_pages + 1):
+        client.lpush(
+            "kym_bootstrap_queue:start_urls",
+            f"https://knowyourmeme.com/memes/popular/page/{page}",
+        )
+
+
+if __name__ == "__main__":
+    feed(10, "redis://redis:6379")
